74e01953e256677b1991e3b0de7537d8762c2604
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-02-20 10:49:06 +0530

    Update config.py

--- SiestaRobot/config.py
+++ SiestaRobot/config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/SiestaRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/IchigoRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -18,8 +18,8 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 945137470  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "saint_foire"
-    SUPPORT_CHAT = "machinaxsupport"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "baby_hoii"
+    SUPPORT_CHAT = "ichigosupportchat"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001180007354
     )  # Prints any new group the bot is added to, prints just the name and ID.
