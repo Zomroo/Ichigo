2c8ce6e19654bf5e7cb16336e9289fc579dc7fdc
Husbando <103307099+Husbandoo@users.noreply.github.com>
2022-05-08 01:58:26 +0530

    Update __init__.py

--- SiestaRobot/__init__.py
+++ SiestaRobot/__init__.py
@@ -40,7 +40,7 @@ logging.getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLeve
 LOGGER = logging.getLogger('[IchigoRobot]')
 LOGGER.info("ichigo is starting. | An academy Project Parts. | Licensed under GPLv3.")
 LOGGER.info("Not affiliated to other anime or Villain in any way whatsoever.")
-LOGGER.info("Project maintained by: github.com/shiinobu (t.me/baby_hoii)")
+LOGGER.info("Project maintained by: github.com/shiinobu (t.me/Husbandoo)")
 
 # if version < 3.9, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 9:
